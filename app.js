592fbcbed6ff2297a7613e4c2197fe30627b9918
Joshua Eilers <eilers.joshua@gmail.com>
2015-12-24 01:07:07 -0800

    routing

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+var controllers = {};
+
+controllers.SidebarController = function($scope) {
+	$scope.navItems = [
+		{ name: 'About', route: 'about' },
+		{ name: 'Resume', route: 'resume' },
+		{ name: 'Blog', route: 'blog' }
+	];
+	$scope.selected = 0;
+	$scope.select = function(index) {
+		$scope.selected = index;
+	};
+};
+
+controllers.AboutController = function($scope) {
+	console.log('about controller');
+};
+
+controllers.ResumeController = function($scope) {
+	console.log('resume controller');
+};
+
+controllers.BlogController = function($scope) {
+	console.log('blog controller');
+};
+
+app.controller(controllers);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'AboutController',
+      templateUrl: 'about.html'
+    })
+    .when('/about', {
+      controller: 'AboutController',
+      templateUrl: 'about.html'
+    })
+    .when('/resume', {
+    	controller: 'ResumeController',
+    	templateUrl: 'resume.html'
+    })
+    .when('/blog', {
+    	controller: 'BlogController',
+    	templateUrl: 'blog.html'
+    })
+    .otherwise({ redirectTo: '/' });
+});
