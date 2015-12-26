f7e0347e5f2dd858c407763393e0e86e977a8d6c
Joshua Eilers <eilers.joshua@gmail.com>
2015-12-25 17:06:56 -0800

    updates to blog

--- app.js
+++ app.js
@@ -18,7 +18,24 @@ controllers.ResumeController = function($scope) {
 };
 
 controllers.BlogController = function($scope) {
-	console.log('blog controller');
+  $scope.posts = [
+    {
+      title: 'Lorem Ipsume Whatever',
+      preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper malesuada ante sit amet lobortis. In hac habitasse platea dictumst. Mauris pretium pharetra orci, a porta quam. Mauris eget aliquam orci. Duis sollicitudin congue odio nec dictum. Quisque semper sit amet dui at accumsan. Ut molestie, tortor sit amet ultrices pharetra, magna risus placerat dui, et scelerisque tellus neque eget dui. Nullam vulputate et metus nec interdum. Praesent nec vehicula nisl.',
+      date: new Date(),
+      tags: ['Tag1', 'Tag2', 'Tag3'],
+      collection: 'Collection1',
+      commentCount: 31
+    },
+    {
+      title: 'Lorem Ipsume Whatever 2',
+      preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper malesuada ante sit amet lobortis. In hac habitasse platea dictumst. Mauris pretium pharetra orci, a porta quam. Mauris eget aliquam orci. Duis sollicitudin congue odio nec dictum. Quisque semper sit amet dui at accumsan. Ut molestie, tortor sit amet ultrices pharetra, magna risus placerat dui, et scelerisque tellus neque eget dui. Nullam vulputate et metus nec interdum. Praesent nec vehicula nisl.',
+      date: new Date(),
+      tags: ['Tag4', 'Tag5', 'Tag6'],
+      collection: 'Collection2',
+      commentCount: 1337
+    }
+  ];
 };
 
 app.controller(controllers);
