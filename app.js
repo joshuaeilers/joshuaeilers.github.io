76506c412ad0fd6661eaffe3708810a74be5a5a0
Joshua Eilers <eilers.joshua@gmail.com>
2015-12-26 21:34:42 -0800

    elapsed time in posts

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 var app = angular.module('app', ['ngRoute']);
 
+var services = {};
+
+services.elapsedTimeService = function() {
+  this.oneSecond = 1000;
+  this.oneMinte = this.oneSecond * 60;
+  this.oneHour = 60 * this.oneMinte;
+  this.oneDay = this.oneHour * 24;
+  this.oneWeek = this.oneDay * 7;
+  this.oneMonth = this.oneDay * 30;
+  this.oneYear = this.oneDay * 365;
+
+  this.calculate = function(begin, end) {
+    var scale = 1;
+    var unit = 'second';
+    var a = begin.getTime();
+    var b = end.getTime();
+    var diff = b - a;
+
+    if ((scale = diff / this.oneYear) >= 1) {
+      unit = 'year';
+    } else if ((scale = diff / this.oneMonth) >= 1) {
+      unit = 'month';
+    } else if ((scale = diff / this.oneWeek) >= 1) {
+      unit = 'week';
+    } else if ((scale = diff / this.oneDay) >= 1) {
+      unit = 'day';
+    } else if ((scale = diff / this.oneHour) >= 1) {
+      unit = 'hour';
+    } else if ((scale = diff / this.oneMinte) >= 1) {
+      unit = 'minute';
+    } else if ((scale = diff / this.oneSecond) >= 1) {
+      unit = 'second';
+    } else {
+      scale = 1;
+      unit = 'second';
+    }
+
+    return { scale: Math.floor(scale), unit: unit };
+  };
+};
+
+app.service(services);
+
 var controllers = {};
 
 controllers.SidebarController = function($scope, $location) {
@@ -17,25 +60,15 @@ controllers.ResumeController = function($scope) {
 	console.log('resume controller');
 };
 
-controllers.BlogController = function($scope) {
-  $scope.posts = [
-    {
-      title: 'Lorem Ipsume Whatever',
-      preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper malesuada ante sit amet lobortis. In hac habitasse platea dictumst. Mauris pretium pharetra orci, a porta quam. Mauris eget aliquam orci. Duis sollicitudin congue odio nec dictum. Quisque semper sit amet dui at accumsan. Ut molestie, tortor sit amet ultrices pharetra, magna risus placerat dui, et scelerisque tellus neque eget dui. Nullam vulputate et metus nec interdum. Praesent nec vehicula nisl.',
-      date: new Date(),
-      tags: ['Tag1', 'Tag2', 'Tag3'],
-      collection: 'Collection1',
-      commentCount: 31
-    },
-    {
-      title: 'Lorem Ipsume Whatever 2',
-      preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper malesuada ante sit amet lobortis. In hac habitasse platea dictumst. Mauris pretium pharetra orci, a porta quam. Mauris eget aliquam orci. Duis sollicitudin congue odio nec dictum. Quisque semper sit amet dui at accumsan. Ut molestie, tortor sit amet ultrices pharetra, magna risus placerat dui, et scelerisque tellus neque eget dui. Nullam vulputate et metus nec interdum. Praesent nec vehicula nisl.',
-      date: new Date(),
-      tags: ['Tag4', 'Tag5', 'Tag6'],
-      collection: 'Collection2',
-      commentCount: 1337
+controllers.BlogController = function($scope, $http, elapsedTimeService) {
+  $http.get('data/posts.json').then(function(response) {
+    var data = response.data;
+    var now = new Date();
+    for (var i = 0, l = data.length; i < l; i++) {
+      data[i].elapsed = elapsedTimeService.calculate(new Date(data[i].date), now);
     }
-  ];
+    $scope.posts = data;
+  });
 };
 
 app.controller(controllers);
